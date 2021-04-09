7ad3616a98bd822c9852c8262df6651fde8c5234
Madhan M <madhanmandy3374@gmail.com>
2021-04-09 10:46:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aicte/Employer_SignUp.java
@@ -0,0 +1,14 @@
+package com.example.aicte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Employer_SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employer__sign_up);
+    }
+}
---
+++ app/src/main/java/com/example/aicte/Job_Application.java
@@ -0,0 +1,13 @@
+package com.example.aicte;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Job_Application extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job__application);
+    }
+}
